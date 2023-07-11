6ee0a3e278068ae469976aa7a891a6b1d650b84b
irene <panisirene@gmail.com>
2023-07-10 23:15:58 -0700

    fix category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,7 +66,7 @@ router.delete('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
-    return res.json(categoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
